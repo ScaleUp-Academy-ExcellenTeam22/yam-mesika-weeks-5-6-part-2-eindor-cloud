6bad842a6c6310c26d4e5129e7f1f414d70b4c6c
eindor <eindor13@gmail.com>
2022-04-06 20:39:11 +0300

    comments

--- division.py
+++ division.py
@@ -1,20 +1,24 @@
 
-#find a
-def find_perfect(upper_bound=10_000):
-    for number in range(1, upper_bound):
+#find all the numbers
+#that the sum of their divisors is equal to the number itself
+def find_perfect():
+    number = 0
+    while True:
         sum = 0
+        number += 1
         for divisor in divisorGenerator(number):
             sum += divisor
             if sum == number:
                     yield number
 
-
+#a generator that finds all the divisors of a number
+#I got it from the web
 def divisorGenerator(n):
     for i in range(1, int(n/2+1)):
         if n % i == 0:
             yield i
     yield n
 
-
+#get all perfect numbers using the generator
 for perfect in find_perfect():
     print(perfect)
