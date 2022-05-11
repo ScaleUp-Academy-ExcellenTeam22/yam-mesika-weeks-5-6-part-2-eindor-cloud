98c111a2d8983351b32a555a3671411c3f36c277
eindor <eindor13@gmail.com>
2022-05-11 19:27:15 +0300

    added file

---
+++ main.py
@@ -0,0 +1,23 @@
+#find all the numbers
+#that the sum of their divisors is equal to the number itself
+def find_perfect():
+    number = 0
+    while True:
+        sum = 0
+        number += 1
+        for divisor in divisorGenerator(number):
+            sum += divisor
+            if sum == number:
+                    yield number
+
+#a generator that finds all the divisors of a number
+#I got it from the web
+def divisorGenerator(n):
+    for i in range(1, int(n/2+1)):
+        if n % i == 0:
+            yield i
+    yield n
+
+#get all perfect numbers using the generator
+for perfect in find_perfect():
+    print(perfect)
